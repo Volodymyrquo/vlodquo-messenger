b66a1f146feb0c6fecb2784b92b56bf5b0cdd36b
Volodymyr Basok <volodymyrquo@gmail.com>
2021-06-14 09:58:28 +0300

    deleted sumralogged and sumramatrix components

--- src/components/structures/SumraLoggedInView.js
+++
@@ -1,5 +0,0 @@
-/* import LoggedInView from "matrix-react-sdk/src/components/structures/LoggedInView";
-
-export default class SumraLoggedInView extends LoggedInView {
-    static replaces = "LoggedInView";
-} */

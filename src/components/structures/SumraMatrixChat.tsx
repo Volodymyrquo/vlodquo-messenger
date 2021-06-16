13078b4b596059b23571c55fa4c29c105237a418
Volodymyr Basok <volodymyrquo@gmail.com>
2021-06-16 18:42:43 +0300

    added logiInView to customization folder

---
+++ src/components/structures/SumraMatrixChat.tsx
@@ -0,0 +1,10 @@
+
+/* 
+import MatrixChat from "matrix-react-sdk/src/components/structures/MatrixChat";
+
+export default class SumraMatrixChat extends  MatrixChat {
+    static replaces = "MatrixChat";
+
+
+} */
+
